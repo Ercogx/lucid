3d5ca78d2cd0ecb1322d8a734387dcf4538c8bfb
Ercogx <ercogx@gmail.com>
2020-07-10 17:20:57 +0300

    End of project

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,37 @@ document.addEventListener("DOMContentLoaded", function() {
 	// Custom JS
 	$('.testimonials-carousel').owlCarousel({
     loop:true,
+    margin: 25,
     responsive:{
         0:{
             items:1
         },
-        1000:{
+        992:{
             items:2
         }
     }
-	})
+	});
+
+
+	$(".navbar-nav").on("click",".nav-link", function (event) {
+		event.preventDefault();
+		let id  = $(this).attr('href'),
+			timeScroll = 1500,
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, timeScroll);
+		if($(window).width() < 768){
+			setTimeout(() => {
+				$("#navbarSupportedContent").collapse('hide');
+			}, timeScroll)
+		}
+	});
+
+	$("#navbarSupportedContent").on("show.bs.collapse", function(){
+		$('body').css('overflow', 'hidden');
+	});
+	$("#navbarSupportedContent").on("hide.bs.collapse", function(){
+		$('body').css('overflow', 'scroll');
+	});
+
+
 });
